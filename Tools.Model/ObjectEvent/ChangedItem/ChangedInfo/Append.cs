c01f6ec96c3a16904d51c920df9fe2cef768734a
Ansi <the-ansi@yandex.ru>
2015-10-05 12:28:37 +0300

    modify date

---
+++ Tools.Model/ObjectEvent/ChangedItem/ChangedInfo/Append.cs
@@ -0,0 +1,9 @@
+﻿namespace PeletonSoft.Tools.Model.ObjectEvent.ChangedItem.ChangedInfo
+{
+    public class AppendItemChangedInfo : ItemChangedInfo
+    {
+
+        public override int RecalculateIndex(int index) => index;
+        public override bool IsEmptyChanged(int count) => count == 1;
+    }
+}
