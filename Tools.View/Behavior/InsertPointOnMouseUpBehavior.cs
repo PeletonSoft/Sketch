5e03d870109525c90ddf2385e24c9fec776e8bd2
Ansi <the-ansi@yandex.ru>
2015-10-07 13:44:25 +0300

    sync adding file

---
+++ Tools.View/Behavior/InsertPointOnMouseUpBehavior.cs
@@ -0,0 +1,65 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+using PeletonSoft.Tools.Model.Dragable;
+using PeletonSoft.Tools.Model.Draw;
+
+namespace PeletonSoft.Tools.View.Behavior
+{
+    public class InsertPointOnMouseUpBehavior : Behavior<UIElement>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.MouseUp += MouseUp;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.MouseUp -= MouseUp;
+            base.OnDetaching();
+        }
+
+        private void MouseUp(object sender, MouseButtonEventArgs mouseButtonEventArgs)
+        {
+            InsertCommand?.Execute(new InsertPointTransit(new Point(X, Y), Line));
+        }
+
+        public double X
+        {
+            get { return (double)GetValue(XProperty); }
+            set { SetValue(XProperty, value); }
+        }
+
+        public static readonly DependencyProperty XProperty = DependencyProperty.Register(
+          nameof(X), typeof(double), typeof(InsertPointOnMouseUpBehavior), new PropertyMetadata(0.0));
+
+        public double Y
+        {
+            get { return (double)GetValue(YProperty); }
+            set { SetValue(YProperty, value); }
+        }
+
+        public static readonly DependencyProperty YProperty = DependencyProperty.Register(
+          nameof(Y), typeof(double), typeof(InsertPointOnMouseUpBehavior), new PropertyMetadata(0.0));
+
+
+        public ILineViewModel Line
+        {
+            get { return (ILineViewModel)GetValue(LineProperty); }
+            set { SetValue(LineProperty, value); }
+        }
+
+        public static readonly DependencyProperty LineProperty = DependencyProperty.Register(
+          nameof(Line), typeof(ILineViewModel), typeof(InsertPointOnMouseUpBehavior), new PropertyMetadata(null));
+
+        public ICommand InsertCommand
+        {
+            get { return (ICommand)GetValue(InsertCommandProperty); }
+            set { SetValue(InsertCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty InsertCommandProperty = DependencyProperty.Register(
+          nameof(InsertCommand), typeof(ICommand), typeof(InsertPointOnMouseUpBehavior), new PropertyMetadata(null));
+    }
+}
---
+++ Tools.View/Behavior/MoveInCanvasBehavor.cs
@@ -0,0 +1,79 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+using System.Windows.Media;
+
+namespace PeletonSoft.Tools.View.Behavior
+{
+    public class MoveInCanvasBehavor : Behavior<UIElement>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.MouseMove += MouseMove;
+        }
+
+        protected override void OnDetaching()
+        {
+            AssociatedObject.MouseMove -= MouseMove;
+            base.OnDetaching();
+        }
+
+        private UIElement Control { get; set; }
+        private Canvas Canvas { get; set; }
+
+        private void MouseMove(object sender, MouseEventArgs e)
+        {
+            DependencyObject obj = AssociatedObject;
+            if (Canvas == null)
+            {
+                while (true)
+                {
+                    var cnv = VisualTreeHelper.GetParent(obj);
+
+                    if (cnv is Canvas)
+                    {
+                        Canvas = (Canvas)cnv;
+                        Control = (UIElement)obj;
+                        break;
+                    }
+
+                    if (cnv == null)
+                    {
+                        break;
+                    }
+
+                    obj = cnv;
+                }
+            }
+
+            if (Canvas != null)
+            {
+                var p = e.GetPosition(Canvas);
+                X = p.X - (double)Control.GetValue(Canvas.LeftProperty);
+                Y = p.Y - (double)Control.GetValue(Canvas.TopProperty);
+            }
+
+        }
+
+        public double X
+        {
+            get { return (double)GetValue(XProperty); }
+            set { SetValue(XProperty, value); }
+        }
+
+        public static readonly DependencyProperty XProperty = DependencyProperty.Register(
+          nameof(X), typeof(double), typeof(MoveInCanvasBehavor), new PropertyMetadata(0.0));
+
+        public double Y
+        {
+            get { return (double)GetValue(YProperty); }
+            set { SetValue(YProperty, value); }
+        }
+
+        public static readonly DependencyProperty YProperty = DependencyProperty.Register(
+          nameof(Y), typeof(double), typeof(MoveInCanvasBehavor), new PropertyMetadata(0.0));
+
+    }
+}
