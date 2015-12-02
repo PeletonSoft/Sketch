6d8c3a9c465f5695ee0fffb7e06bfcc052487713
Ansi <the-ansi@yandex.ru>
2015-12-02 17:21:28 +0300

    Add another unit test

--- Sketch.ViewModel.Test/WorkspaceViewModelTest.cs
+++ Sketch.ViewModel.Test/WorkspaceViewModelTest.cs
@@ -4,6 +4,8 @@ using PeletonSoft.Sketch.ViewModel.Factory;
 using PeletonSoft.Sketch.ViewModel.Interface;
 using PeletonSoft.Sketch.ViewModel.Interface.Element;
 using PeletonSoft.Tools.Model.Dependency;
+using PeletonSoft.Tools.Model.Memento;
+using PeletonSoft.Tools.Model.Memento.Serialize;
 using PeletonSoft.Tools.Model.ObjectEvent.ChangedItem;
 using PeletonSoft.Tools.Model.Setting;
 using Rhino.Mocks;
@@ -46,7 +48,71 @@ namespace PeletonSoft.Sketch.ViewModel.Test
             Assert.That(elementList.Collection.Last().Layout.OpacityMask, Is.Null);
             screen.Width = 5;
             Assert.That(elementList.Collection.Last().Layout.OpacityMask, Is.Null);
+        }
+
+        [Test]
+        public void ElementList_AddAllElement_NoError()
+        {
+
+            var factories = new IElementFactoryViewModel<IElementViewModel>[]
+            {
+                new PortiereFactoryViewModel(),
+                new PleatFactoryViewModel(),
+                new TulleFactoryViewModel(),
+                new TieBackFactoryViewModel(), 
+                new PanelFactoryViewModel(), 
+                new HardPelmetFactoryViewModel(), 
+                new ApplicationFactoryViewModel(), 
+                new ScanFactoryViewModel(), 
+                new OverlayFactoryViewModel(), 
+                new FilletFactoryViewModel(), 
+                new EqualSwagFactoryViewModel(), 
+                new EqualTailFactoryViewModel(), 
+                new ScaleneSwagFactoryViewModel(), 
+                new ScaleneTailFactoryViewModel(),
+                new DeJabotFactoryViewModel(), 
+                new LatticeFactoryViewModel(), 
+                new RomanBlindFactoryViewModel()
+            };
+            var screen = new ScreenViewModel()
+            {
+                Width = 4,
+                Height = 2
+            };
+
+
+            var settingData = MockRepository.GenerateStub<ISettingData>();
+            settingData.Stub(sd => sd.ProgramName).Return("MyName");
+            settingData.Stub(sd => sd.Version).Return("1.0.0.0");
+
+            var settingProvider = MockRepository.GenerateStub<ISettingProvider>();
+            settingProvider.Stub(sp => settingProvider.GetSettingData()).Return(settingData);
+
+            var workspace = new WorkspaceViewModel()
+            {
+                Factories = factories,
+                Screen = screen,
+                CommandFactory = MockRepository.GenerateStub<ICommandFactory>(),
+                SettingProvider = settingProvider
+            };
+
+            var elementList = workspace.ElementList;
+            factories.ToList()
+                .ForEach(factory => elementList.AppendElement(factory));
+
+            Assert.That(elementList.Count(), Is.EqualTo(factories.Length));
+
+            var dataTransfer = workspace.Save();
+
+            Assert.DoesNotThrow(() =>
+            {
+                var serializer = new XmlSerializer(StandardXmlPrimitive.Primitives, dataTransfer);
+                serializer.Serialize();
+            });
+            workspace.Restore(dataTransfer);
+            Assert.That(elementList.Count(), Is.EqualTo(factories.Length));
 
         }
+
     }
 }
