cdfca3a55d51913adf876743b4ef8fe3a7e5f85f
Ansi <the-ansi@yandex.ru>
2015-12-02 10:56:18 +0300

    workspace test

--- Sketch.ViewModel.Test/WorkspaceViewModelTest.cs
+++ Sketch.ViewModel.Test/WorkspaceViewModelTest.cs
@@ -1,5 +1,14 @@
-﻿using NUnit.Core;
+﻿using System.Collections.Generic;
+using NUnit.Core;
 using NUnit.Framework;
+using PeletonSoft.Sketch.ViewModel.Factory;
+using PeletonSoft.Sketch.ViewModel.Factory.Custom;
+using PeletonSoft.Sketch.ViewModel.Interface;
+using PeletonSoft.Sketch.ViewModel.Interface.Element;
+using PeletonSoft.Tools.Model.Dependency;
+using PeletonSoft.Tools.Model.ObjectEvent.ChangedItem;
+using PeletonSoft.Tools.Model.Setting;
+using Rhino.Mocks;
 
 namespace PeletonSoft.Sketch.ViewModel.Test
 {
@@ -9,7 +18,34 @@ namespace PeletonSoft.Sketch.ViewModel.Test
         [Test]
         public void ElementList_ThreeLayerAndChangeSize_FreeOpacityMask()
         {
-            Assert.That(1, Is.EqualTo(1));
+            
+            var factories = new IElementFactoryViewModel<IElementViewModel>[]
+            {
+                new PortiereFactoryViewModel(), 
+                new PleatFactoryViewModel(), 
+                new TulleFactoryViewModel()
+            };
+            var screen = new ScreenViewModel()
+            {
+                Width = 4,
+                Height = 2
+            };
+            
+            var workspace = new WorkspaceViewModel()
+            {
+                Factories = factories,
+                Screen = screen,
+                CommandFactory = MockRepository.GenerateStub<ICommandFactory>(),
+                SettingProvider = MockRepository.GenerateStub<ISettingProvider>()
+            };
+
+            var elementList = workspace.ElementList;
+            elementList.AppendElement(factories[0]);
+            elementList.AppendElement(factories[1]);
+            elementList.AppendElement(factories[2]);
+
+            Assert.That(elementList.Count(), Is.EqualTo(3));
+
         }
     }
 }
